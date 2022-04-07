8b7c5ba4694085f0a6bcf3c7d68e8f10700c1913
Mohamed Alaa <36743366+MohamedAlaaEldin636@users.noreply.github.com>
2022-04-07 11:47:33 +0200

    - fixed minor bug

--- app/src/main/java/com/grand/ezkorone/core/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/grand/ezkorone/core/MyFirebaseMessagingService.kt
@@ -2,7 +2,9 @@ package com.grand.ezkorone.core
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.JsonParser
 import com.grand.ezkorone.core.customTypes.NotificationUtils
+import org.json.JSONObject
 import timber.log.Timber
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
@@ -12,8 +14,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Timber.e("message key/value pairs -> ${remoteMessage.data}")
 
-        val title = remoteMessage.data.getValue("title").orEmpty()
-        val body = remoteMessage.data.getValue("body").orEmpty()
+        val (title, body) = remoteMessage.data.getValue("message").orEmpty()
+            .getNotificationModel()
 
         NotificationUtils.showNotificationToLaunchMainActivityForFirebaseNotification(
             this,
@@ -21,5 +23,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             body
         )
     }
+
+    private data class NotificationModel(
+        val title: String,
+        val body: String,
+    )
+
+    private fun String.getNotificationModel(): NotificationModel {
+        val jsonObject = JSONObject(this)
+
+        val title = jsonObject.getString("title")
+        val body = jsonObject.getString("body")
+
+        return NotificationModel(title, body)
+    }
+    /*
+    public static BaseNotificationModel getNotification(String mJsonString) {
+        JsonParser parser = new JsonParser();
+        JsonElement mJson = parser.parse(mJsonString);
+        Gson gson = new Gson();
+        return gson.fromJson(mJson, BaseNotificationModel.class);
+    }
+     */
 
 }
